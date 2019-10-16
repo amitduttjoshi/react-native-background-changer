2cb1923ff41188566cc274b14023164b152d1201
amitduttjoshi <amitduttjoshi@gmail.com>
2019-10-16 08:49:10 +0530

    Updated Background Color Changer

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [state, setState] = useState ({bgColor: '#f90213'});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, {backgroundColor: state.bgColor}]}>
+      <TouchableOpacity onPress={() => setState ({bgColor: randomRGB ()})}>
+        <Text style={styles.btn}>Change Color</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+const randomRGB = () => {
+  const red = Math.floor (Math.random () * 256);
+  const green = Math.floor (Math.random () * 256);
+  const blue = Math.floor (Math.random () * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    color: '#FFF',
+    borderRadius: 5,
+    borderWidth: 0.5,
+    borderColor: '#cccccc',
+    backgroundColor: '#222222',
+    padding: 10,
+    margin: 5,
+  },
 });
+
+export default App;
